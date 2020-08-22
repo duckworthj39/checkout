6dbca7feac0ac85756ab0f3d82b755e9eca21f6d
Jason Duckworth <duckworthj39@gmail.com>
2020-08-22 12:52:28 +0100

    Added item class which holds the items info. Added ability to get checkout total without offers.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,12 +1,22 @@
 class Checkout 
 
+	attr_reader :items
+
+	def initialize
+		@items = Array.new
+	end
 
 	def scan(item)
-		
+		items << item
 	end
 
 	def total
-		3
+		total = 0.00
+
+		items.each do |item| 
+			total += item.price
+		end
+		total
 	end
 
 end
--- spec/checkout_challenge_spec.rb
+++ spec/checkout_challenge_spec.rb
@@ -4,28 +4,48 @@ RSpec.describe Checkout do
 
 
 	let(:promotional_rules) {}
-	let(:co) { Checkout.new(promotional_rules) }
+	let(:co) { Checkout.new }
 
-	it 'returns the correct total for 001, 002, 003' do
-		co.scan(001)
-		co.scan(002)
-		co.scan(003)
-		expect(co.total).to eq(9.25)
-	end 
+	# If I was working on an api these would usually be request specs
+	context 'integration specs' do
+		it 'returns correct total for 001, 002, 003' do
+			# Arrange
+			item1 = Item.new(001, 'Lavender heart', 9.25)
+			item2 = Item.new(002, 'Personalised cufflinks', 45.00)
+			item3 = Item.new(003, 'Kids T-shirt', 19.95)
 
-	it 'returns the correct total for 001, 003, 001' do
-		co.scan(001)
-		co.scan(003)
-		co.scan(001)
-		expect(co.total).to eq(45.00)
+			co.scan(item1)
+			co.scan(item2)
+			co.scan(item3)
+			# Act
+			total = co.total
+			# Assert
+			expect(total).to eq(74.20)
+		end
 	end
 
-	it 'returns the correct total for 001, 002, 001, 003' do
-		co.scan(001)
-		co.scan(002)
-		co.scan(001)
-		co.scan(003)
-		expect(co.total).to eq(19.95)
+	xcontext 'challenge requirements' do
+		it 'returns the correct total for 001, 002, 003' do
+			co.scan(001)
+			co.scan(002)
+			co.scan(003)
+			expect(co.total).to eq(9.25)
+		end
+
+		it 'returns the correct total for 001, 003, 001' do
+			co.scan(001)
+			co.scan(003)
+			co.scan(001)
+			expect(co.total).to eq(45.00)
+		end
+
+		it 'returns the correct total for 001, 002, 001, 003' do
+			co.scan(001)
+			co.scan(002)
+			co.scan(001)
+			co.scan(003)
+			expect(co.total).to eq(19.95)
+		end
 	end
 end
 
